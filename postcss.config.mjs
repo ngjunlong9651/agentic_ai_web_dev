e50085ae3e54431ee3161a556136ccc4b69f8ef3
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2026-03-24 16:47:58 +0800

    Changed the product page

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {
+      content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+    },
   },
 };
 
